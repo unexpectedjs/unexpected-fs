0a99af8c3185b72553ec9688f249d0f8bd2fa718
Gustav Nikolaj Olsen <gno@one.com>
2015-05-30 23:45:10 +0200

    mock.restore

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ module.exports = {
             var extraArgs = Array.prototype.slice.call(arguments, 3);
             return expect.promise(function (resolve, reject) {
                 mockfs(value);
-
                 return resolve(expect.apply(expect, [subject].concat(extraArgs)));
             }).then(function () {
-                // When is this executed?
-                //mockfs.restore();
+                mockfs.restore();
+            }).caught(function (err) {
+                mockfs.restore();
+                throw err;
             });
         });
     }
