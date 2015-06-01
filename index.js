9a9439358650c366046c0596bff683b975ba39d7
Gustav Nikolaj Olsen <gno@one.com>
2015-06-01 20:30:16 +0200

    use mountfs to only mock out parts of the filesystem

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var mockfs = require('mock-fs');
+var fs = require('fs');
+var MountFs = require('mountfs');
 var rewriteMockFsOptions = require('./lib/rewriteMockFsOptions');
 
 module.exports = {
@@ -7,14 +9,34 @@ module.exports = {
         expect.addAssertion('with fs mocked out', function (expect, subject, value) {
             this.errorMode = 'bubble';
             var extraArgs = Array.prototype.slice.call(arguments, 3);
-            var mockOptions = rewriteMockFsOptions(value);
-            return expect.promise(function (resolve, reject) {
-                mockfs(mockOptions);
-                return resolve(expect.apply(expect, [subject].concat(extraArgs)));
+
+            var mockFileSystems = Object.keys(value).map(function (key) {
+                var mockFsConfig = rewriteMockFsOptions(value[key]);
+                return {
+                    mountPath: key,
+                    fileSystem: mockfs.fs(mockFsConfig)
+                };
+            });
+
+            var unmountAllMocks = function () {
+                mockFileSystems.forEach(function (mockFs) {
+                    fs.unmount(mockFs.mountPath);
+                });
+                fs.unpatch();
+            };
+
+            return expect.promise(function () {
+                MountFs.patchInPlace();
+
+                mockFileSystems.forEach(function (mockFileSystem) {
+                    fs.mount(mockFileSystem.mountPath, mockFileSystem.fileSystem);
+                });
+
+                return expect.apply(expect, [subject].concat(extraArgs));
             }).then(function () {
-                mockfs.restore();
+                unmountAllMocks();
             }).caught(function (err) {
-                mockfs.restore();
+                unmountAllMocks();
                 throw err;
             });
         });
--- test/index.spec.js
+++ test/index.spec.js
@@ -6,7 +6,7 @@ describe('unexpected-fs', function () {
         var fileContent = function (fileName) {
             var fs = require('fs');
 
-            return fs.readFileSync('/' + fileName, 'utf-8');
+            return fs.readFileSync('/data/' + fileName, 'utf-8');
         };
 
         var expect = unexpected
@@ -15,7 +15,9 @@ describe('unexpected-fs', function () {
 
         it('should not throw', function () {
             return expect('foobar.txt', 'with fs mocked out', {
-                '/foobar.txt': 'Foobar!'
+                '/data/': {
+                    '/foobar.txt': 'Foobar!'
+                }
             }, 'when passed as parameter to', fileContent, 'to equal', 'Foobar!');
         });
 
@@ -28,13 +30,66 @@ describe('unexpected-fs', function () {
                         ctime: new Date(1)
                     });
                 }, 'with fs mocked out', {
-                    '/foo.txt': {
-                        _isFile: true,
-                        ctime: new Date(1),
-                        content: 'foobar!'
+                    '/': {
+                        '/foo.txt': {
+                            _isFile: true,
+                            ctime: new Date(1),
+                            content: 'foobar!'
+                        }
                     }
                 }, 'not to throw');
             });
+        });
+        it('should allow reading files on the normal fs', function () {
+            return expect(function () {
+                var path = require('path');
+                var fs = require('fs');
+                var packageJsonPath = path.resolve(__dirname, '..', 'package.json');
+                var packageJson = fs.readFileSync(packageJsonPath, 'utf-8');
+            }, 'with fs mocked out', {}, 'not to throw');
+        });
+        it('should allow reading files on different mounted mock fs', function () {
+            var path = require('path');
+            var mockFs = {};
+            mockFs[path.resolve(__dirname, 'wtf') + '/'] = {
+                '/FUNNYSTORY.md': 'lol, just kidding.'
+            };
+            mockFs[path.resolve(__dirname, 'lol') + '/'] = {
+                '/SADSTORY.md': 'wtf, just kidding?'
+            };
+            return expect(function () {
+                var fs = require('fs');
+                var path = require('path');
+                var funnyStoryPath = path.resolve(__dirname, 'wtf', 'FUNNYSTORY.md');
+                var funnyStory = fs.readFileSync(funnyStoryPath, 'utf-8');
+                expect(funnyStory, 'to equal', 'lol, just kidding.');
+                var sadStoryPath = path.resolve(__dirname, 'lol', 'SADSTORY.md');
+                var sadStory = fs.readFileSync(sadStoryPath, 'utf-8');
+                expect(sadStory, 'to equal', 'wtf, just kidding?');
+            }, 'with fs mocked out', mockFs, 'not to throw');
+        });
+        it('should allow reading both files on the real fs and the mocked out one', function () {
+            var path = require('path');
+            var mockFs = {};
+            mockFs[path.resolve(__dirname, 'wtf') + '/'] = {
+                '/FUNNYSTORY.md': 'lol, just kidding.'
+            };
+            mockFs[path.resolve(__dirname, 'lol') + '/'] = {
+                '/SADSTORY.md': 'wtf, just kidding?'
+            };
+            return expect(function () {
+                var fs = require('fs');
+                var path = require('path');
+                var packageJsonPath = path.resolve(__dirname, '..', 'package.json');
+                var packageJson = fs.readFileSync(packageJsonPath, 'utf-8');
+                expect(packageJson, 'to match', /^{\s+"name":\s"unexpected-fs",/);
+                var funnyStoryPath = path.resolve(__dirname, 'wtf', 'FUNNYSTORY.md');
+                var funnyStory = fs.readFileSync(funnyStoryPath, 'utf-8');
+                expect(funnyStory, 'to equal', 'lol, just kidding.');
+                var sadStoryPath = path.resolve(__dirname, 'lol', 'SADSTORY.md');
+                var sadStory = fs.readFileSync(sadStoryPath, 'utf-8');
+                expect(sadStory, 'to equal', 'wtf, just kidding?');
+            }, 'with fs mocked out', mockFs, 'not to throw');
         });
     });
     describe('does it update already exisitng fs modules?', function () {
@@ -48,7 +103,9 @@ describe('unexpected-fs', function () {
                 .clone()
                 .installPlugin(unexpectedFs);
             return expect('doesIt.txt', 'with fs mocked out', {
-                '/doesIt.txt': 'Yes it does!'
+                '/': {
+                    '/doesIt.txt': 'Yes it does!'
+                }
             }, 'when passed as parameter to', someMethod, 'to equal', 'Yes it does!');
         });
     });
